84ddaafacdd706fe5e85f4321b74e07e680b4bc2
akashkharde <akashkharde111@gmail.com>
2024-06-18 23:11:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ExpenceTracker from "./ExpenceTrackerProject/ExpenceTracker"
+
+function App() {
+  return (
+    <div className="App">
+     <ExpenceTracker/>
+    </div>
+  );
+}
+
+export default App;
